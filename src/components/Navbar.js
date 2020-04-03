e60f3986a69075f0a67a536d57188e1fa98eb360
Sarantha <ssmahadoowage@gmail.com>
2020-04-03 11:14:22 +0530

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,20 +9,24 @@ export default class Navbar extends Component {
         isOpen:false
     }
     handelToggle = () => {
-        this.setState({isOpen:!this.state.isOpen})
+        this.setState({isOpen:!this.state.isOpen});
     };
 
-    render () {
+    render() {
         return (
             <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-hader">
                         <Link to="/"><img src={logo} alt="Beach Resort"></img></Link>
-                        <button type="button" className="nav-btn" onClick={this.handelToggle}><FaAlignRight class="nav-icon"/></button>
+                        <button type="button" style={{float:'right',paddingTop:'8px'}} className="nav-btn" onClick={this.handelToggle}><FaAlignRight class="nav-icon"/></button>
                     </div>
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/rooms">Rooms</Link></li>
+                    </ul>
                 </div>
-                
             </nav>
-        );
+        )
     }
 }
+
